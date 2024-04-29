66d0587be50419669fbca8cc6efe7810e77b9ddc
Esra Aydogan <esraydgn0842@gmail.com>
2024-04-29 13:58:01 +0300

    ..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //yönlendirmeleri tanımlamızı sağlıyor
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from './header';
+import ImageSlider from './pages/ImageSlider'; 
+import image1 from './image/1.resim.jpg';
+import image2 from './image/2.resim.jpg';
+import image3 from './image/3.resim.jpg';
+import image4 from './image/4.resim.jpg';
 
+const images = [image1, image2, image3, image4];
 
-export const App = () =>{
+export const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-    </>
-    );
-  }
-  export default App;
+      <ImageSlider images={images} /> {/* ImageSlider komponentini burada render edin */}
+      <Routes>
+        {/* Yönlendirmelerinizi burada tanımlayın */}
+        {/* Örnek: <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
