dca0e0d6204bd7d59aae320b675536429e8a3fb6
Esra Aydogan <esraydgn0842@gmail.com>
2024-05-07 19:17:24 +0300

    results fixed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from './Header.js';
 import { LoginPage } from './pages/Login/LoginPage.jsx';
 import { HomePage } from './pages/HomePage.js';
-import { CareerQuiz } from './pages/CareerQuiz.js';
+import { CareerQuiz } from './pages/CareerQuiz/CareerQuiz.js';
 import { Departments } from './pages/Departments/Departments.js';
 import { Universities } from './pages/Universities/Universities.js';
+import { Results } from './pages/CareerQuiz/Results.js';
 
 export const App = () => {
   return (
@@ -15,6 +16,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/careerquiz" element={<CareerQuiz/>} />
+        <Route path="/careerquiz/results" element={<Results/>} />
         <Route path="/loginpage" element={<LoginPage/>} />
         <Route path="/departmens" element={<Departments/>} />
         <Route path="/universities" element={<Universities/>} />
