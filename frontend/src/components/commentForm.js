ee102ac1d8a79165c00445206e8d431c0ff183ee
PNarTN <pinartaylan07@gmail.com@google.com>
2024-06-26 21:53:48 +0300

    .

--- frontend/src/components/commentForm.js
+++ frontend/src/components/commentForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './commentForm.css';
+import { toast } from 'react-toastify';
+
 
 const CommentForm = ({ universityId, onNewComment, onClose }) => {
   const [username, setUsername] = useState('');
@@ -9,11 +11,29 @@ const CommentForm = ({ universityId, onNewComment, onClose }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newComment = { universityId, username, comment };
-    const res = await axios.post('http://localhost:8000/comments', newComment);
-    onNewComment(res.data);
-    setUsername('');
-    setComment('');
+
+    try {
+      const res = await axios.post('http://localhost:8000/comments', newComment);
+      onNewComment(res.data);
+      setUsername('');
+      setComment('');
+      toast.success('Yorum kaydedildi!'); // Başarılı bildirim
+    } catch (error) {
+      console.error('Error adding comment:', error);
+      toast.error('Yorum eklenirken bir hata oluştu.'); // Hata bildirimi
+    }
   };
+  //   try{
+  //   const res = await axios.post('http://localhost:8000/comments', newComment);
+  //   onNewComment(res.data);
+  //   setUsername('');
+  //   setComment('');
+  //   toast.success('Yorum kaydedildi!'); // Başarılı bildirim
+  //   } catch (error) {
+  //     console.error('Error adding comment:', error);
+  //     toast.error('Yorum eklenirken bir hata oluştu.'); // Hata bildirimi
+  //   }
+  // };
 
   return (
     <div className="comment-form-container">
