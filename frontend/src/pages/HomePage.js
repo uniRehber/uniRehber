4c7af2e42af8ddc8516bd82e4d7cfbbccddc0e6f
Esra Aydogan <esraydgn0842@gmail.com>
2024-06-28 09:40:23 +0300

    exams

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,6 +5,7 @@ import image1 from '../image/1.resim.jpg';
 import image2 from '../image/2.resim.jpg';
 import image3 from '../image/3.resim.jpg';
 import image4 from '../image/4.resim.jpg';
+import './HomePage.css'; // CSS dosyasını dahil edin
 
 const images = [
   { src: image1, alt: "Ösym KPSS 2024 Lisans KPSS 2024 önlisans KPSS 2024 ortaoğretim" },
@@ -17,9 +18,18 @@ export const HomePage = () => {
   return (
     <div>
       <ImageSlider images={images} />
+      <div className="exam-buttons">
+        <h2>Geçmiş Yılların YKS Sınavları</h2>
+        <button onClick={() => window.open('/exams/2024_yks_ayt.pdf', '_blank')}>2024 YKS AYT</button>        
+        <button onClick={() => window.open('/exams/2024_yks_tyt.pdf', '_blank')}>2024 YKS TYT</button>
+        <button onClick={() => window.open('/exams/2023_yks_ayt.pdf', '_blank')}>2023 YKS AYT</button>
+        <button onClick={() => window.open('/exams/2023_yks_tyt.pdf', '_blank')}>2023 YKS TYT</button>
+        <button onClick={() => window.open('/exams/2022_yks_ayt.pdf', '_blank')}>2022 YKS AYT</button>
+        <button onClick={() => window.open('/exams/2022_yks_tyt.pdf', '_blank')}>2022 YKS TYT</button>
+      </div>
       <AboutPage />
     </div>
-  )
+  );
 }
 
 export default HomePage;
